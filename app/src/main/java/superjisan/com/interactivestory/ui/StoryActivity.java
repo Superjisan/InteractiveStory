e21f93c9ab27e2da3152d6c79deaad1d432664db
Jisan Zaman <jzaman@wesleyan.edu>
2015-03-05 19:59:15 -0500

    fixed input being already set to being at hint

--- app/src/main/java/superjisan/com/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/superjisan/com/interactivestory/ui/StoryActivity.java
@@ -2,11 +2,16 @@ package superjisan.com.interactivestory.ui;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import superjisan.com.interactivestory.R;
 import superjisan.com.interactivestory.model.Page;
+import superjisan.com.interactivestory.model.Story;
 
 
 public class StoryActivity extends Activity {
@@ -14,17 +19,47 @@ public class StoryActivity extends Activity {
     private Page[] mPages;
 
     public static final String TAG = StoryActivity.class.getSimpleName();
+
+    private Story mStory = new Story();
+    private ImageView mImageView;
+    private TextView mTextView;
+    private Button mChoice1;
+    private Button mChoice2;
+    private String mName;
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
 
         Intent intent = getIntent();
-        String name = intent.getStringExtra(getString(R.string.key_name));
-        if (name == null) {
-            name = "Friend";
+        mName = intent.getStringExtra(getString(R.string.key_name));
+        if (mName == null) {
+            mName = "Friend";
         }
 
-        Log.d(TAG, name);
+        mImageView = (ImageView)findViewById(R.id.storyImageView);
+        mTextView = (TextView)findViewById(R.id.storyTextView);
+        mChoice1 = (Button)findViewById(R.id.choiceButton1);
+        mChoice2 = (Button)findViewById(R.id.choiceButton2);
+
+        loadPage();
+    }
+
+    private void loadPage(){
+        Page page = mStory.getPage(0);
+
+        Drawable drawable = getResources().getDrawable(page.getImageId());
+
+        mImageView.setImageDrawable(drawable);
+
+        String pageText = page.getText();
+        //add the name if placeholder included Won't add if not placeholder
+        pageText = String.format(pageText, mName);
+        mTextView.setText(pageText);
+
+        mChoice1.setText(page.getChoice1().getText());
+        mChoice2.setText(page.getChoice2().getText());
     }
 
 
