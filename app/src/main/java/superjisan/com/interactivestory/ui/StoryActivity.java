ace859cf0b7d8adb1f9564b35858f5995a5195f7
Jisan Zaman <jzaman@wesleyan.edu>
2015-03-05 20:28:28 -0500

    all the storyline is fixed

--- app/src/main/java/superjisan/com/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/superjisan/com/interactivestory/ui/StoryActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -26,6 +27,7 @@ public class StoryActivity extends Activity {
     private Button mChoice1;
     private Button mChoice2;
     private String mName;
+    private Page mCurrentPage;
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,23 +45,55 @@ public class StoryActivity extends Activity {
         mChoice1 = (Button)findViewById(R.id.choiceButton1);
         mChoice2 = (Button)findViewById(R.id.choiceButton2);
 
-        loadPage();
+        loadPage(0);
     }
 
-    private void loadPage(){
-        Page page = mStory.getPage(0);
+    private void loadPage(int choice){
+        mCurrentPage = mStory.getPage(choice);
 
-        Drawable drawable = getResources().getDrawable(page.getImageId());
+        Drawable drawable = getResources().getDrawable(mCurrentPage.getImageId());
 
         mImageView.setImageDrawable(drawable);
 
-        String pageText = page.getText();
+        String pageText = mCurrentPage.getText();
+
+
+
         //add the name if placeholder included Won't add if not placeholder
         pageText = String.format(pageText, mName);
         mTextView.setText(pageText);
 
-        mChoice1.setText(page.getChoice1().getText());
-        mChoice2.setText(page.getChoice2().getText());
+        if(mCurrentPage.getIsFinal()){
+            mChoice1.setVisibility(View.INVISIBLE);
+            mChoice2.setText("PLAY AGAIN");
+            mChoice2.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view) {
+                    finish();
+                }
+            });
+        }else {
+
+
+            mChoice1.setText(mCurrentPage.getChoice1().getText());
+            mChoice2.setText(mCurrentPage.getChoice2().getText());
+
+            mChoice1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int nextPage = mCurrentPage.getChoice1().getNextPage();
+                    loadPage(nextPage);
+                }
+            });
+
+            mChoice2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int nextPage = mCurrentPage.getChoice2().getNextPage();
+                    loadPage(nextPage);
+                }
+            });
+        }
     }
 
 
